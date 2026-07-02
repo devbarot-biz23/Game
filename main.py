1b670dc1c73d1738915819ab685711fd7cd35dbc
Dev Barot <dev.barot@biznovare.com>
2026-07-02 16:43:26 +0530

    Add Goblin and Tower classes with unique IDs and spawn functionality

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from poetry.console.commands import self
 
 class Goblin:
+    Counter_N = 0
     def __init__(self):
+        Goblin.Counter_N += 1
+        self.id = "E"+str(Goblin.Counter_N)
         self.pos = 0
         self.health = 3
         self.speed = 1
+        print(f"Spawned { self.id } goblin on lane1 at position {self.pos}")
+
 
     def move(self):
         self.pos += self.pos
@@ -20,10 +25,15 @@ class Goblin:
         pass
 
 class Tower:
+    Counter_N = 0
     def __init__(self):
+        Tower.Counter_N += 1
+        self.id = "T"+str(Tower.Counter_N)
         self.pos = 0
         self.range = 1
 
+    def place(self, index):
+        self.pos = index
 
 class Base:
     def __init__(self):
@@ -38,11 +48,25 @@ class Base:
     def __del__(self):
         pass
 
-def spawn_enemy(pos : int, type : str):
+def spawn_enemy(type : str):
+
     if type == "Goblin":
         Goblin()
 
 def main():
-    str = 0
-    while(str):
-        input("Enter a string")
+    start = True
+    print("hello")
+    while start:
+        command = input()
+        if command.startswith("Spawn"):
+            enemy = command.split(" ")[2]
+            spawn_enemy(enemy)
+        if command.startswith("ADD_TOWER"):
+            pos = command.split(" ")[2]
+            tower = Tower()
+        if command.startswith("EXIT"):
+            start = False
+
+
+if __name__ == "__main__":
+    main()
