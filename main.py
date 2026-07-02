ffdc4341ec3327aeceafa1dad3667619fad3c2ee
Dev Barot <dev.barot@biznovare.com>
2026-07-02 18:03:28 +0530

    Fix Goblin movement logic, enhance tower spawn message, and add game status printing

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ class Goblin:
         pass
 
     def move(self):
-        self.pos += self.pos
+        self.pos += 1
         if self.pos > 5:
             del self
             return
@@ -42,7 +42,7 @@ class Tower:
         self.id = "T"+str(Tower.Counter_N)
         self.pos = position
         self.range = 1
-        print(f"Spawned { self.id } tower on lane1 at position {self.pos}")
+        print(f"Added { self.id } tower on lane1 at position {self.pos}")
 
 
     def place(self, index):
@@ -69,6 +69,40 @@ def spawn_enemy(types : str):
         GAME.enemy_track.append(Goblin())
 
 
+def print_status():
+    e_list1 = []
+    e_list2 = []
+    e_list3 = []
+    e_list4 = []
+    e_list5 = []
+    t_list1 = []
+    t_list2 = []
+    t_list3 = []
+    t_list4 = []
+    t_list5 = []
+    for e_obj in GAME.enemy_track:
+        if e_obj.pos == 0:
+            e_list1.append(e_obj.id)
+        if e_obj.pos == 1:
+            e_list2.append(e_obj.id)
+        if e_obj.pos == 2:
+            e_list3.append(e_obj.id)
+        if e_obj.pos == 3:
+            e_list4.append(e_obj.id)
+        if e_obj.pos == 4:
+            e_list5.append(e_obj.id)
+    for t_obj in GAME.tower_track:
+        if t_obj.pos == 0:
+            t_list1.append(t_obj.id)
+        if t_obj.pos == 1:
+            t_list2.append(t_obj.id)
+        if t_obj.pos == 2:
+            t_list3.append(t_obj.id)
+        if t_obj.pos == 3:
+            t_list4.append(t_obj.id)
+        if t_obj.pos == 4:
+            t_list5.append(t_obj.id)
+    print(e_list1, t_list1, "----", e_list2, t_list2, "----", e_list3, t_list3, "----", e_list4, t_list4, "----",e_list5, t_list5)
 
 GAME = Game()
 BASE = Base()
@@ -89,23 +123,28 @@ def main():
             count_space = len(GAME.enemy_track) + len(GAME.tower_track)
             print("lane1")
             print(" 0 " + " " * count_space + " 1 " + " " * count_space + " 2 "+ " " * count_space + " 3 "+ " " * count_space + " 4 "+ " " * count_space + " BASE ")
+            print_status()
         if command.startswith("EXIT"):
             start = False
         if command.startswith("RUN_TURN"):
             GAME.turns += 1
+            print(f"TURN : {GAME.turns} started")
+
             for t_obj in GAME.tower_track:
                 for e_obj in GAME.enemy_track:
                     if e_obj.pos == t_obj.pos or e_obj.pos == t_obj.pos-1 or e_obj.pos == t_obj.pos+1:
                         e_obj.damage()
                         print(f"{t_obj.id} attacked {e_obj.pos} for 1 damage, {e_obj.id} hp={e_obj.health}")
                         break
-            for e_obj in GAME.tower_track:
-                e_obj.move()
-            for e_obj in GAME.tower_track:
+            for e_obj in GAME.enemy_track:
                 if e_obj.pos == 5:
                     BASE.damage()
 
-            print(f"TURN : {GAME.turns} started")
+            for e_obj in GAME.enemy_track:
+                e_obj.move()
+        else:
+            pass
+
 
 
 
