c262a140ffd318cc2efbd6a9e01c10e3bc73ce3a
Dev Barot <dev.barot@biznovare.com>
2026-07-02 15:50:46 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+class Goblin:
+    def __init__(self):
+        self.pos = 0
+        self.health = 3
+        self.speed = 1
+
+    def move(self):
+        self.pos += self.pos
+        if self.pos > 5:
+            del self
+
+    def health(self):
+        self.health -= 1
+        if self.health < 0:
+            del self
+
+    def __del__(self):
+        pass
+
+def spawn_enemy(pos : int, type : str):
+    if type == "Goblin":
+        Goblin()
+
+def main():
+    lane = {1}
+    input("Enter a string")
