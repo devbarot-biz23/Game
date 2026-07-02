9135c8d4ea199b5e5ccafb27121f91e7e42b21fa
Dev Barot <dev.barot@biznovare.com>
2026-07-02 16:11:41 +0530

    Add initial classes

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from poetry.console.commands import self
+
 class Goblin:
     def __init__(self):
         self.pos = 0
@@ -22,10 +24,25 @@ class Tower:
         self.pos = 0
         self.range = 1
 
+
+class Base:
+    def __init__(self):
+        self.health = 10
+
+    def damage(self):
+        self.health -= 1
+        if self.health < 0:
+            print("GAME OVER")
+            del self
+
+    def __del__(self):
+        pass
+
 def spawn_enemy(pos : int, type : str):
     if type == "Goblin":
         Goblin()
 
 def main():
-    lane = {1}
-    input("Enter a string")
+    str = 0
+    while(str):
+        input("Enter a string")
